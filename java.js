49ced14495c322f6b6ddc819ad839e73cbe787c9
kemex10 <61971140+kemex10@users.noreply.github.com>
2020-04-14 09:27:13 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,31 @@
+$('body').mouseover(function(){
+    $(this).css({cursor: 'none'});
+  });
+  
+  $(document).on('mousemove', function(e){
+    $('#cursor').css({
+      left:  e.pageX,
+      top:   e.pageY
+    });
+  });
+
+  //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
